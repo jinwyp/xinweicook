94f98e0af1088af48842cb66aee2c12a4eb0e449
JinWYP <jinwyp@gmail.com>
2015-07-21 17:42:25 +0800

    del scr

--- src/js/module.js
+++
@@ -1,5 +0,0 @@
-angular.module('RDash', ['ui.bootstrap', 'ui.router', 'RDash.config', 'RDash.models']);
-
-angular.module('RDash.config', ['ngStorage', 'ui.router']);
-
-angular.module('RDash.models', ['ngStorage', 'restangular']);
