fe8fbd25ff688709df064b413242d9219e5857eb
JinWYP <jinwyp@gmail.com>
2015-07-29 14:34:39 +0800

    add

--- app/libs/alipay.js
+++ app/libs/alipay.js
@@ -28,3 +28,31 @@ var mid = function(req,res,next){
         next();
     });
 };
+
+
+var a = {
+    paymentWeixinpay : {
+        code_url   : undefined,
+        prepay_id  : 'wx201507291424493fb57c4ff60654415824',
+        trade_type : 'JSAPI',
+        sign       : 'DB50748CB01423AEA8DDF55AD5748BA7',
+        nonce_str  : 'CglcZsmsAURjlVON',
+        mobileSign : {
+            paySign   : 'ED4613F35255A22C600CF789A4373EB4',
+            signType  : 'MD5',
+            package   : 'prepay_id=wx201507291424493fb57c4ff60654415824',
+            nonceStr  : 'CglcZsmsAURjlVON',
+            timeStamp : '1438151090',
+            appId     : 'wx37a1323e488cef84'
+        },
+        nativeSign : {
+            sign         : '713E9FD6880B523961A3ABB1D8878D1C',
+            timeStamp    : '1438151090',
+            nonceStr     : 'CglcZsmsAURjlVON',
+            packageValue : 'Sign=WXPay',
+            prepayId     : 'wx201507291424493fb57c4ff60654415824',
+            partnerId    : '1260182401',
+            appId        : 'wxc31508f4ded1402b'
+        }
+    }
+}
