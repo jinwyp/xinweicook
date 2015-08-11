fd7b6b1f64f3f78c4049a1a9e85e39389fc517a6
ykc <ykc@yuekechengdeMacBook-Pro.local>
2015-08-11 14:41:49 +0800

    update

--- app/public/mobile/src/js/controllers/fav-list.js
+++ app/public/mobile/src/js/controllers/fav-list.js
@@ -1,3 +1,19 @@
-angular.module('xw.controllers').controller('favCtrl', function ($scope, User, Dishes) {
+angular.module('xw.controllers').controller('favCtrl', function ($scope, User, ScopeDecorator) {
 
+    ScopeDecorator.common($scope);
+
+    function init() {
+        User.getUserInfo().then(function (res) {
+            var dishList = res.data.dishLikeList;
+            $scope.cookList = dishList.filter(function (dish) {
+                return dish.cookingType == 'ready to cook';
+            });
+
+            $scope.eatList = dishList.filter(function (dish) {
+                return dish.cookingType == 'ready to eat';
+            })
+        })
+    }
+
+    init();
 })
