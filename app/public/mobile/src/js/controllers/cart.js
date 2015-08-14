4856798d448496a0dd21ca48a6993fd0ef5b6149
ykc <ykc@yuekechengdeMacBook-Pro.local>
2015-08-14 11:08:48 +0800

    添加购物车

---
+++ app/public/mobile/src/js/controllers/cart.js
@@ -0,0 +1,29 @@
+angular.module('xw.controllers').controller('cartCtrl', function ($scope, User, ScopeDecorator) {
+    ScopeDecorator.common($scope);
+
+    var postCart = {};
+
+    function init() {
+        User.getUserInfo().then(function (res) {
+            $scope.cart = res.data.shoppingCart;
+
+            postCart.shoppingCart = $scope.cart.map(function (dish) {
+                var newDish = {
+                    dish: dish._id,
+                    number: dish.number
+                };
+                if (dish.subDish && dish.subDish.length) {
+                    newDish.subDish = dish.subDish.map(function (el) {
+                        return {
+                            dish: el._id,
+                            number: el.number
+                        }
+                    })
+
+                }
+            })
+        })
+    }
+
+    init();
+});
