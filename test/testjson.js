2970401cf20ad053e2bba32a73a0cc12569fac38
Jin <jinwyp@gmail.com>
2015-08-19 22:36:52 +0800

    fixed bug for dish details

--- test/testjson.js
+++ test/testjson.js
@@ -3,67 +3,66 @@
  */
 
 
-user = {
-    "address":[
+var user = {
+    "address" : [
         {
             "geoLatitude" : 20, // 纬度
             "geoLongitude" : 20, // 纬度
 
             "country" : "china",
-            "province": "shanghai",
-            "city": "shanghai",
-            "district": "shanghai",
+            "province" : "shanghai",
+            "city" : "shanghai",
+            "district" : "shanghai",
             "street" : "枫林路",
-            "address": "510号",
+            "address" : "510号",
 
             "isTemporary" : false,  //临时地址 下单后变为false
-            "isDefault":  false,
+            "isDefault" : false,
 
-            "contactPerson": "xinwei",
-            "mobile": "13564568304",
-            "alias": "",
-            "remark": ""
+            "contactPerson" : "xinwei",
+            "mobile" : "13564568304",
+            "alias" : "",
+            "remark" : ""
         }
     ]
-}
+};
 
 
-dish =
-{
+var dish = {
     "shoppingCart" : [
         {
-            "dish": "558a602a3eba152266ff2b8c",
-            "number": 1,
+            "dish" : "558a602a3eba152266ff2b8c",
+            "number" : 1,
             "subDish" : [
                 {
-                    "dish": "5583b7faa2845dec35276b92",
-                    "number": 1
+                    "dish" : "5583b7faa2845dec35276b92",
+                    "number" : 1
                 },
                 {
-                    "dish": "5583b7faa2845dec35276b95",
-                    "number": 1
+                    "dish" : "5583b7faa2845dec35276b95",
+                    "number" : 1
                 },
                 {
-                    "dish": "5583b7faa2845dec35276b97",
-                    "number": 2
+                    "dish" : "5583b7faa2845dec35276b97",
+                    "number" : 2
                 }
             ]
         },
         {
-            "dish": "558a602a3eba152266ff2b8c",
-            "number": 1,
+            "dish" : "558a602a3eba152266ff2b8c",
+            "number" : 1,
             "subDish" : [
                 {
-                    "dish": "5583b7faa2845dec35276b92",
-                    "number": 1
+                    "dish" : "5583b7faa2845dec35276b92",
+                    "number" : 1
                 },
                 {
-                    "dish": "5583b7faa2845dec35276b95",
-                    "number": 1
+                    "dish" : "5583b7faa2845dec35276b95",
+                    "number" : 1
                 },
                 {
-                    "dish": "5583b7faa2845dec35276b97",
-                    "number": 2
+                    "dish" : "5583b7faa2845dec35276b97",
+                    "number" : 2
                 }
             ]
         }
@@ -71,10 +70,8 @@ dish =
 };
 
 
+var order = {
 
-
-order =
-{
     "cookingType" : "ready to cook",
     "clientFrom" : "ios",
     "credit" : 0,
@@ -95,52 +92,52 @@ order =
         "geoLongitude" : 30,
 
         "country" : "",
-        "province": "shanghai",
-        "city": "shanghai",
-        "district": "shanghai" ,
-        "street" : "shanghai" ,
-        "address": "xxxxx" ,
+        "province" : "shanghai",
+        "city" : "shanghai",
+        "district" : "shanghai",
+        "street" : "shanghai",
+        "address" : "xxxxx",
 
-        "contactPerson": "wangyupeng" ,
-        "mobile": "13564568304" ,
-        "remark": "comment"
+        "contactPerson" : "wangyupeng",
+        "mobile" : "13564568304",
+        "remark" : "comment"
     },
 
 
     "dishList" : [
         {
-            "dish": "558a602a3eba152266ff2b8c",
-            "number": 1,
+            "dish" : "558a602a3eba152266ff2b8c",
+            "number" : 1,
             "subDish" : [
                 {
-                    "dish": "5583b7faa2845dec35276b92",
-                    "number": 1
+                    "dish" : "5583b7faa2845dec35276b92",
+                    "number" : 1
                 },
                 {
-                    "dish": "5583b7faa2845dec35276b95",
-                    "number": 1
+                    "dish" : "5583b7faa2845dec35276b95",
+                    "number" : 1
                 },
                 {
-                    "dish": "5583b7faa2845dec35276b97",
-                    "number": 2
+                    "dish" : "5583b7faa2845dec35276b97",
+                    "number" : 2
                 }
             ]
         },
         {
-            "dish": "558a602a3eba152266ff2b8c",
-            "number": 1,
+            "dish" : "558a602a3eba152266ff2b8c",
+            "number" : 1,
             "subDish" : [
                 {
-                    "dish": "5583b7faa2845dec35276b92",
-                    "number": 1
+                    "dish" : "5583b7faa2845dec35276b92",
+                    "number" : 1
                 },
                 {
-                    "dish": "5583b7faa2845dec35276b95",
-                    "number": 1
+                    "dish" : "5583b7faa2845dec35276b95",
+                    "number" : 1
                 },
                 {
-                    "dish": "5583b7faa2845dec35276b97",
-                    "number": 2
+                    "dish" : "5583b7faa2845dec35276b97",
+                    "number" : 2
                 }
             ]
         }
@@ -148,75 +145,159 @@ order =
 };
 
 
+
+
+
 var alipay1 = {
-    discount            : '0.00',
-    payment_type        : '1',
-    subject             : 'XinWei_ReadyToCook',
-    trade_no            : '2015073000001000020058528698',
-    buyer_email         : '18629641521',
-    gmt_create          : '2015-07-30 17:31:33',
-    notify_type         : 'trade_status_sync',
-    quantity            : '1',
-    out_trade_no        : '201507301731239646112',
-    seller_id           : '2088111042213083',
-    notify_time         : '2015-07-30 17:31:33',
-    body                : 'XinWeiCook',
-    trade_status        : 'WAIT_BUYER_PAY',
+    discount : '0.00',
+    payment_type : '1',
+    subject : 'XinWei_ReadyToCook',
+    trade_no : '2015073000001000020058528698',
+    buyer_email : '18629641521',
+    gmt_create : '2015-07-30 17:31:33',
+    notify_type : 'trade_status_sync',
+    quantity : '1',
+    out_trade_no : '201507301731239646112',
+    seller_id : '2088111042213083',
+    notify_time : '2015-07-30 17:31:33',
+    body : 'XinWeiCook',
+    trade_status : 'WAIT_BUYER_PAY',
     is_total_fee_adjust : 'Y',
-    total_fee           : '0.01',
-    seller_email        : 'steve.ge@me.com',
-    price               : '0.01',
-    buyer_id            : '2088412033326020',
-    notify_id           : 'fb25d93cb256758edbf9015e3b6a2a0724',
-    use_coupon          : 'N',
-    sign_type           : 'RSA',
-    sign                : 'HcV9Tl2ii4Kh7872WYbHNpUjOz5Slh03P0cF4nVFzkUFbKKby9PMPdC1qhKIDTP1kK/gAp3kdao1tQ+A0VsmGgW4UGHnbLLKJBYDtsPuChO2BMxjIVCCWMT84fGJJeBI2QewzE6vWrMRuTct6ewkObOTEQUA2OC6FrUuFECOv90='
+    total_fee : '0.01',
+    seller_email : 'steve.ge@me.com',
+    price : '0.01',
+    buyer_id : '2088412033326020',
+    notify_id : 'fb25d93cb256758edbf9015e3b6a2a0724',
+    use_coupon : 'N',
+    sign_type : 'RSA',
+    sign : 'HcV9Tl2ii4Kh7872WYbHNpUjOz5Slh03P0cF4nVFzkUFbKKby9PMPdC1qhKIDTP1kK/gAp3kdao1tQ+A0VsmGgW4UGHnbLLKJBYDtsPuChO2BMxjIVCCWMT84fGJJeBI2QewzE6vWrMRuTct6ewkObOTEQUA2OC6FrUuFECOv90='
 };
 
 
 var alipay2 = {
-    discount            : '0.00',
-    payment_type        : '1',
-    subject             : 'XinWei_ReadyToCook',
-    trade_no            : '2015073000001000020058530883',
-    buyer_email         : '18629641521',
-    gmt_create          : '2015-07-30 17:56:57',
-    notify_type         : 'trade_status_sync',
-    quantity            : '1',
-    out_trade_no        : '201507301756061517782',
-    seller_id           : '2088111042213083',
-    notify_time         : '2015-07-30 17:56:58',
-    body                : 'XinWeiCook',
-    trade_status        : 'TRADE_SUCCESS',
+    discount : '0.00',
+    payment_type : '1',
+    subject : 'XinWei_ReadyToCook',
+    trade_no : '2015073000001000020058530883',
+    buyer_email : '18629641521',
+    gmt_create : '2015-07-30 17:56:57',
+    notify_type : 'trade_status_sync',
+    quantity : '1',
+    out_trade_no : '201507301756061517782',
+    seller_id : '2088111042213083',
+    notify_time : '2015-07-30 17:56:58',
+    body : 'XinWeiCook',
+    trade_status : 'TRADE_SUCCESS',
     is_total_fee_adjust : 'N',
-    total_fee           : '0.01',
-    gmt_payment         : '2015-07-30 17:56:58',
-    seller_email        : 'steve.ge@me.com',
-    price               : '0.01',
-    buyer_id            : '2088412033326020',
-    notify_id           : '3ef4dbf66b02100660c8bc9cdb670ef224',
-    use_coupon          : 'N',
-    sign_type           : 'RSA',
-    sign                : 'Bp0ahbYpzjMLLb+oBVz5u8nk606VH374tkA06Z7xXBynEAHjEPc109aKdi4mWDgoj/G8MZWmrX9WFL31Rg6Qu8JyJNd0FkivDuSFs51SYTJIokazwScBHOu35hDNRcuE4PIiL1cVDno7+aHHVa5pH2uS9FNDmAZGaSl/Wk33b/o='
+    total_fee : '0.01',
+    gmt_payment : '2015-07-30 17:56:58',
+    seller_email : 'steve.ge@me.com',
+    price : '0.01',
+    buyer_id : '2088412033326020',
+    notify_id : '3ef4dbf66b02100660c8bc9cdb670ef224',
+    use_coupon : 'N',
+    sign_type : 'RSA',
+    sign : 'Bp0ahbYpzjMLLb+oBVz5u8nk606VH374tkA06Z7xXBynEAHjEPc109aKdi4mWDgoj/G8MZWmrX9WFL31Rg6Qu8JyJNd0FkivDuSFs51SYTJIokazwScBHOu35hDNRcuE4PIiL1cVDno7+aHHVa5pH2uS9FNDmAZGaSl/Wk33b/o='
 };
 
 
 var weixinpay = {
-    appid          : 'wxc31508f4ded1402b',
-    attach         : '55cb142f1ce9a11b3b29a3e1',
-    bank_type      : 'CFT',
-    cash_fee       : '1',
-    fee_type       : 'CNY',
-    is_subscribe   : 'N',
-    mch_id         : '1260182401',
-    nonce_str      : 'VyrZo041MlpySrO3',
-    openid         : 'o-SvZwvBW32tW3WE6ltyeEgfN-hM',
-    out_trade_no   : '201508121738550015511',
-    result_code    : 'SUCCESS',
-    return_code    : 'SUCCESS',
-    sign           : '357591EB80607E8F3FEE007CFCA6112F',
-    time_end       : '20150812174031',
-    total_fee      : '1',
-    trade_type     : 'APP',
+    appid : 'wxc31508f4ded1402b',
+    attach : '55cb142f1ce9a11b3b29a3e1',
+    bank_type : 'CFT',
+    cash_fee : '1',
+    fee_type : 'CNY',
+    is_subscribe : 'N',
+    mch_id : '1260182401',
+    nonce_str : 'VyrZo041MlpySrO3',
+    openid : 'o-SvZwvBW32tW3WE6ltyeEgfN-hM',
+    out_trade_no : '201508121738550015511',
+    result_code : 'SUCCESS',
+    return_code : 'SUCCESS',
+    sign : '357591EB80607E8F3FEE007CFCA6112F',
+    time_end : '20150812174031',
+    total_fee : '1',
+    trade_type : 'APP',
     transaction_id : '1008770927201508120606427576'
-}
+};
+
+
+var testDish = {
+    "_id"             : "55d177b7d7886b10327769a2",
+    "modifiedAt"      : "2015-08-19T05:56:22.889Z",
+    "createdAt"       : "2015-08-17T05:57:11.637Z",
+    "autoIncrementId" : 10283,
+    "sortId"          : 1010,
+    "cookingType"     : "ready to cook",
+    "sideDishType"    : "main",
+    "setType"         : "single",
+    "difficulty"      : 2,
+    "time"            : 20,
+    "servings"        : 1,
+    "storageLife"     : 2,
+    "priceOriginal"   : 58,
+
+    "statisticViews"        : 0,
+    "statisticLikeUserList" : [],
+    "statisticLike"         : 0,
+    "statisticSales"        : 0,
+    "statisticHot"          : 0,
+    "stock"                 : 20,
+    "recommendSet"          : [],
+    "topping"               : [],
+    "preferences"           : [],
+    "priceWholesale"        : [],
+    "tagFilter"             : ["5590d256103f46d9ac31e3e2", "5590d256103f46d9ac31e3f1", "5590d256103f46d9ac31e3f9", "5590d256103f46d9ac31e3f3"],
+    "region"                : [],
+    "infoCookingStep"       : [
+        {"contentType" : "pic", "_id" : "55d177b7d7886b10327769a8", "sortId" : 10, "value" : {"zh" : "https://dn-xinweicook.qbox.me/油封鸭腿s-_MG_2166.jpg", "en" : "https://dn-xinweicook.qbox.me/油封鸭腿s-_MG_2166.jpg"}, "title" : {"en" : "", "zh" : ""}},
+        {"contentType" : "txt", "_id" : "55d41a861fe33ffe6c717817", "sortId" : 10, "value" : {"zh" : "秋葵去蒂切片，小胡萝卜、玉米笋对半切开再切片。", "en" : "Cut okra into diamond shape. Cut baby carrots and baby corns in half and then slice them."}, "title" : {"zh" : "准备工作", "en" : "Preparation work"}},
+        {"contentType" : "pic", "_id" : "55d41a861fe33ffe6c717816", "sortId" : 10, "value" : {"zh" : "https://dn-xinweicook.qbox.me/油封鸭腿s-_MG_2184.jpg", "en" : "https://dn-xinweicook.qbox.me/油封鸭腿s-_MG_2184.jpg"}},
+        {"contentType" : "txt", "_id" : "55d41a861fe33ffe6c717815", "sortId" : 10, "value" : {"zh" : "汤锅加水，大火烧开，放入小胡萝卜煮约2分钟后捞出。继续放入秋葵煮约1分钟，捞出。最后放入玉米笋煮约半分钟，捞出。", "en" : "Add water to the pot and bring to boil, put in baby carrots and boil for 2 minutes, take out. Then put in okra, boil for 1 minute and take out. Finally, put in baby corns, boil for half minute and take out."}, "title" : {"zh" : "煮蔬菜", "en" : "Boil the vegetables"}},
+        {"contentType" : "pic", "_id" : "55d41a861fe33ffe6c717814", "sortId" : 10, "value" : {"zh" : "https://dn-xinweicook.qbox.me/油封鸭腿s-_MG_2196.jpg", "en" : "https://dn-xinweicook.qbox.me/油封鸭腿s-_MG_2196.jpg"}},
+        {"contentType" : "txt", "_id" : "55d41a861fe33ffe6c717813", "sortId" : 10, "value" : {"en" : "Heat the pan over low heat, pour in all taro puree, stir and heat for about 1 minute, take out and set aside.(You may also microwave it on high heat for 2 minutes.)", "zh" : "不粘锅小火热锅，倒入香芋泥，加热搅拌约1分钟，取出待用。（或用微波炉高火热2分钟亦可）"}, "title" : {"zh" : "加热香芋泥", "en" : "Heat the taro puree"}},
+        {"contentType" : "pic", "_id" : "55d41a861fe33ffe6c717812", "sortId" : 10, "value" : {"zh" : "https://dn-xinweicook.qbox.me/油封鸭腿s-_MG_2224.jpg", "en" : "https://dn-xinweicook.qbox.me/油封鸭腿s-_MG_2224.jpg"}},
+        {"contentType" : "txt", "_id" : "55d41a861fe33ffe6c717811", "sortId" : 10, "value" : {"zh" : "平底锅中火烧旺，倒入1/3的橄榄油，放入小胡萝卜、秋葵、玉米笋翻炒约1分钟取出，期间撒1/2茶匙盐、1/3茶匙黑胡椒碎调味。", "en" : "Heat the pan over medium heat, pour in 1/3 of the olive oil, add in okra, baby carrots and baby corns, stir-fry for about 1 minute and remove from heat. Meanwhile, add 1/2 teaspoon of salt and 1/3 teaspoon of black pepper to season."}, "title" : {"zh" : "炒蔬菜", "en" : "Stir-fry the vegetables"}},
+        {"contentType" : "pic", "_id" : "55d41a861fe33ffe6c717810", "sortId" : 10, "value" : {"zh" : "https://dn-xinweicook.qbox.me/油封鸭腿s-_MG_2239.jpg", "en" : "https://dn-xinweicook.qbox.me/油封鸭腿s-_MG_2239.jpg"}},
+        {"contentType" : "txt", "_id" : "55d41a861fe33ffe6c71780f", "sortId" : 10, "value" : {"en" : "Wipe the pan, add the remaining olive oil on low heat. Melt butter in the pan. Flatten the garlic and  add them to the pan, add thyme. Sear the duck leg  skin side down for 2 minutes then flip side and sear for another 4-5 minutes. Scoop butter on the duck leg continuously. Finally, switch to high heat to color the duck leg  dark brown.", "zh" : "平底锅简单擦拭后倒入剩余橄榄油，小火热锅，加入黄油至融化。用刀将大蒜拍扁后与百里香一起放入锅中，放入鸭腿，先煎鸭腿带皮一面约2分钟再翻面。期间不断将锅中热油浇在鸭腿上，持续约4-5分钟，最后用大火将鸭腿煎至呈棕色。"}, "title" : {"en" : "Sear the duck leg", "zh" : "煎鸭腿"}},
+        {"contentType" : "pic", "_id" : "55d41a861fe33ffe6c71780e", "sortId" : 10, "value" : {"zh" : "https://dn-xinweicook.qbox.me/油封鸭腿s-_MG_2307.jpg", "en" : "https://dn-xinweicook.qbox.me/油封鸭腿s-_MG_2307.jpg"}},
+        {"contentType" : "txt", "_id" : "55d41a861fe33ffe6c71780d", "sortId" : 10, "value" : {"en" : "Cut the duck leg in half, sprinkle with chocolate sauce and lingonberry jam (you may heat it up if you like), lay on your favorite plate with taro puree and serve!", "zh" : "鸭腿切半，淋上巧克力酱（可根据个人口味加热）和小红莓果酱，与香芋泥一起装入盘中，即可享用！"}, "title" : {"en" : "Plate your dish", "zh" : "摆盘"}}
+    ],
+    "infoIngredient"        : [
+        {"contentType" : "txt", "linkTo" : "", "_id" : "55d177b7d7886b10327769ae", "sortId" : 10, "value" : {"zh" : "", "en" : ""}, "title" : {"zh" : "2茶匙 盐", "en" : "2 Teaspoon Salt"}},
+        {"_id" : "55d177b7d7886b10327769ad", "sortId" : 10, "title" : {"zh" : "3汤匙 橄榄油", "en" : "3 Tablespoon Olive Oil"}},
+        {"_id" : "55d177b7d7886b10327769ac", "sortId" : 10, "title" : {"zh" : "2汤匙 小红莓果酱", "en" : "2 Tablespoon Lingonberry Jam"}},
+        {"_id" : "55d177b7d7886b10327769ab", "sortId" : 10, "title" : {"zh" : "1包 巧克力酱", "en" : "1 Pack Chocolate Sauce"}},
+        {"_id" : "55d177b7d7886b10327769aa", "sortId" : 10, "title" : {"zh" : "1包 百里香", "en" : "1 Pack Thyme"}},
+        {"_id" : "55d177b7d7886b10327769a9", "sortId" : 10, "title" : {"zh" : "1包 蔬菜包", "en" : "1 Pack Mixed Vegetable"}},
+        {"_id" : "55d41a861fe33ffe6c71781c", "sortId" : 10, "title" : {"zh" : "1茶匙 黑胡椒碎", "en" : "1 Teaspoon Black Pepper"}},
+        {"_id" : "55d41a861fe33ffe6c71781b", "sortId" : 10, "title" : {"zh" : "2汤匙 黄油", "en" : "2 Tablespoon Butter"}},
+        {"_id" : "55d41a861fe33ffe6c71781a", "sortId" : 10, "title" : {"zh" : "1包 香芋泥", "en" : "1 Pack Taro Puree"}},
+        {"_id" : "55d41a861fe33ffe6c717819", "sortId" : 10, "title" : {"zh" : "2颗 大蒜粒", "en" : "2 Chopped Garlic"}},
+        {"_id" : "55d41a861fe33ffe6c717818", "sortId" : 10, "title" : {"zh" : "1包 鸭腿", "en" : "1 Pack Duck Leg"}}
+    ],
+    "infoUniqueFeature"     : [
+        {"contentType" : "pic", "_id" : "55d177b7d7886b10327769af", "sortId" : 10, "value" : {"en" : "https://dn-xinweicook.qbox.me/油封鸭腿shutterstock_132057926-小.jpg", "zh" : "https://dn-xinweicook.qbox.me/油封鸭腿shutterstock_132057926-小.jpg"}, "title" : {"en" : "", "zh" : ""}},
+        {"contentType" : "txt", "_id" : "55d41a861fe33ffe6c71781d", "sortId" : 10, "value" : {"en" : "A fresh interpretation of the traditional French duck confit. Marinated duck confit slow braised with garden-fresh vegetables and chef’s special chocolate and cranberry sauce, tender with rich flavors.", "zh" : "油封鸭腿是起源于法国保存食材的方法而延伸出来的一道菜。油封（confit）就是把肉泡在油脂中用低温小火慢慢煮熟的一种烹饪手法。大量的油脂可以把肉密封，隔绝空气，从而保持肉的柔嫩口感。新味的油封鸭腿，将鸭腿用鸭油、百里香、杜松果、大蒜、海盐等低温慢煮120分钟，鸭肉软嫩柔滑肥而不腻。"}, "title" : {"zh" : "油封鸭腿", "en" : "French Duck Leg Confit"}}
+    ],
+    "kitchenware"           : [
+        {"zh" : "pan", "en" : "pan", "_id" : "55d177b7d7886b10327769b1"},
+        {"zh" : "pot", "en" : "pot", "_id" : "55d177b7d7886b10327769b0"}
+    ],
+    "cover"                 : [
+        {"zh" : "https://dn-xinweicook.qbox.me/油封鸭腿s-_MG_2307.jpg", "en" : "https://dn-xinweicook.qbox.me/油封鸭腿s-_MG_2307.jpg", "_id" : "55d177b7d7886b10327769b2"},
+        {"zh" : "https://dn-xinweicook.qbox.me/油封鸭腿s-_MG_2309.jpg", "en" : "https://dn-xinweicook.qbox.me/油封鸭腿s-_MG_2309.jpg", "_id" : "55d41a861fe33ffe6c71781e"}
+    ],
+    "brief"                 : {
+        "zh" : "传统法式油封鸭腿新味玩出新花样。精心炮制的油封鸭腿小火慢煎，搭配爽脆的新鲜蔬菜，佐以新味独家秘制的巧克力酱汁和小红莓酱汁，鸭肉柔嫩入味，带给你不一样的全新感受。",
+        "en" : "A fresh interpretation of the traditional French duck confit. Marinated duck confit slow braised with garden-fresh vegetables and chef’s special chocolate and cranberry sauce, tender with rich flavors."
+    },
+    "title"                 : {
+        "zh" : "法式油封鸭腿配红莓酱和巧克力汁",
+        "en" : "French Duck Leg Confit with Lingonberry Jam and Chocolate Sauce"
+    },
+    "isFromAdminPanel"      : true,
+    "isPublished"           : true,
+    "publishedAt"           : "2015-08-17T05:57:11.616Z",
+    "__v"                   : 1
+};
