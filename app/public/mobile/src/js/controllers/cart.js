434eb66e847583e1fc89d0f0782a437d0dd3bdf0
ykc <yuekec@gmail.com>
2015-08-27 12:06:37 +0800

    update

--- app/public/mobile/src/js/controllers/cart.js
+++ app/public/mobile/src/js/controllers/cart.js
@@ -1,4 +1,4 @@
-angular.module('xw.controllers').controller('cartCtrl', function ($scope, User, ScopeDecorator) {
+angular.module('xw.controllers').controller('cartCtrl', function ($scope, User, ScopeDecorator, $localStorage) {
     ScopeDecorator.common($scope);
 
 
@@ -107,8 +107,34 @@ angular.module('xw.controllers').controller('cartCtrl', function ($scope, User, 
     };
 
     $scope.makeOrder = function () {
+        var lists = {cookList: null, eatList: null};
+        var orderLists = {cookList: null, eatList: null};
 
-    }
+        Object.keys(lists).forEach(function (name) {
+            var list = lists[name] = $scope[name].filter(function (el) {return !!el.selected});
+            var cookingType = name == 'cookList' ? 'ready to cook' : 'ready to eat';
+            var orderList = orderLists[name] = postCart.filter(function (el) {
+                return !!el.cookingType == cookingType;
+            });
+
+            // 从orderList中删掉那些不在list中的子菜品
+            for (var i = 0; i < orderLists.length;) {
+                var dish = orderList[i];
+                if (!list.some(function (el) {return el.dish._id == dish.dish;})) {
+                    orderList.splice(i, 1);
+                } else if (dish.subDish) {
+                    for (var j = 0; j < dish.subDish.length;) {
+                        var sDish = dish.subDish[j];
+                        if (!list.some(function (el) {return el.subDish._id == sDish.dish;})) {
+                            dish.subDish.splice(j, 1);
+                        } else j++;
+                    }
+                } else i++;
+            }
+        });
+
+        $localStorage.cart = orderLists;
+    };
 
     function outOfStock (dish) {
         return dish.outOfStock || !dish.isPublished;
@@ -151,17 +177,24 @@ angular.module('xw.controllers').controller('cartCtrl', function ($scope, User, 
                 }
             });
 
+            // postCart 与 订单提交时的结构是一致的.
             postCart = cart.map(function (el) {
                 var dish = el.dish;
                 var newDish = {
                     dish: dish._id,
-                    number: el.number
+                    number: el.number,
+                    cookingType: dish.cookingType,
+                    title: dish.title,
+                    priceOriginal: dish.priceOriginal
                 };
                 if (el.subDish && el.subDish.length) {
                     newDish.subDish = el.subDish.map(function (item) {
                         return {
                             dish: item.dish._id,
-                            number: item.number
+                            number: item.number,
+                            cookingType: item.dish.cookingType,
+                            title: item.dish.title,
+                            priceOriginal: item.dish.priceOriginal
                         }
                     })
 
