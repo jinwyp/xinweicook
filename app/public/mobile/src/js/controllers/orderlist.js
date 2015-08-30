745bad89f4e589676ed02f609789d0ea9add0ed0
ykc <yuekec@gmail.com>
2015-08-30 15:56:18 +0800

    未支付显示主订单，已支付显示子订单

--- app/public/mobile/src/js/controllers/orderlist.js
+++ app/public/mobile/src/js/controllers/orderlist.js
@@ -4,7 +4,14 @@ function orderlistCtrl($scope, Orders) {
 
     function init() {
         Orders.getList().then(function (res) {
-            $scope.orders = res.data;
+            $scope.orders = res.data.filter(function (order) {
+                // 未支付的订单, 过滤isChildOrder:true; 已支付的显示子订单, 过滤isSplitOrder:true
+                if (order.status == 'not paid') {
+                    return !order.isChildOrder
+                } else {
+                    return !order.isSplitOrder
+                }
+            })
         })
     }
 
