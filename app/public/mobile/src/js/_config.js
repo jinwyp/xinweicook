fae43c00df06f1f1efcf66a853d31de7594e2805
ykc <yuekec@gmail.com>
2015-09-02 17:00:36 +0800

    增加cook详情页标题，修复登录401后刷新页面的问题

--- app/public/mobile/src/js/_config.js
+++ app/public/mobile/src/js/_config.js
@@ -1,6 +1,6 @@
 angular.module('xw.config').factory('commonInterceptor', ['$localStorage', '$q', function($localStorage, $q) {
     var noRedirectPath = [/^\/mobile\/$/, /^\/mobile\/login/, /^\/mobile\/cook/];
-    var noRedirectAPI = ['/api/user'];
+    var noRedirectAPI = ['/api/user', '/api/user/token'];
     var loginRedirectPath = ['/mobile/me', '/mobile/addresslist',
         '/mobile/invite', '/mobile/coupons', '/mobile/cook'];
 
--- app/public/mobile/src/js/config.js
+++ app/public/mobile/src/js/config.js
@@ -27,7 +27,7 @@ return {
 });
 angular.module('xw.config').factory('commonInterceptor', ['$localStorage', '$q', function($localStorage, $q) {
     var noRedirectPath = [/^\/mobile\/$/, /^\/mobile\/login/, /^\/mobile\/cook/];
-    var noRedirectAPI = ['/api/user'];
+    var noRedirectAPI = ['/api/user', '/api/user/token'];
     var loginRedirectPath = ['/mobile/me', '/mobile/addresslist',
         '/mobile/invite', '/mobile/coupons', '/mobile/cook'];
 
