48685efa93a735f087fb5449fdf34fbfa72f70c2
ykc <yuekec@gmail.com>
2015-09-09 16:37:42 +0800

    fix createdAt undefined bug

--- app/public/admin/src/js/controllers/order.js
+++ app/public/admin/src/js/controllers/order.js
@@ -328,7 +328,8 @@ function orderController($scope, $timeout, $state, $stateParams, $localStorage, 
 
     if ($state.current.data.type === 'list'){
         $scope.searchOrderCount();
-        $scope.data.searchDateFrom = $scope.data.searchOptions.createdAt.substring(2)
+        var createdAt = $scope.data.searchOptions.createdAt || '';
+        $scope.data.searchDateFrom = createdAt.substring(2)
     }
 
     if ($state.current.data.type === 'update'){
