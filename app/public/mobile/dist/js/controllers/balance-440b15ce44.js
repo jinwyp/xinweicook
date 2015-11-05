e5853bd7b1d9a748fa49116b11361822ff0fef13
JinWYP <jinwyp@gmail.com>
2015-11-05 11:14:24 +0800

    delete

--- app/public/mobile/dist/js/controllers/balance-440b15ce44.js
+++
@@ -1,5 +0,0 @@
-angular.module('xw.controllers').controller('balanceCtrl', function ($scope, Balance, Debug) {
-    Balance.balance().then(function (res) {
-        $scope.balance = res.data.balance;
-    }).catch(Debug.promiseErrFn('查询余额失败'));
-});
--- app/public/mobile/dist/js/controllers/balance-records-b788d6709a.js
+++
@@ -1,5 +0,0 @@
-angular.module('xw.controllers').controller('balanceRecordsCtrl', function ($scope, Balance) {
-    Balance.balanceRecords().then(function (res) {
-        $scope.records = res.data;
-    })
-});
