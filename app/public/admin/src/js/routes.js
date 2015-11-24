ab42eb934456f200d0237d7a8a093e1e880c3c45
JinWYP <jinwyp@gmail.com>
2015-11-24 18:20:08 +0800

    remove todo

--- app/public/admin/src/js/routes.js
+++ app/public/admin/src/js/routes.js
@@ -13,7 +13,7 @@ angular.module('RDash').factory('commonInterceptor', ['$localStorage', '$q', '$l
         'responseError': function(response) {
             // do something on error
             if (response.status == 401 || response.status == 403) {
-                // todo: redirect
+
                 $location.url('/login')
 
             }
