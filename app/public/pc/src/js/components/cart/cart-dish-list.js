8e9a073d3634b8725dd05d39b0abffabd64174bd
ykc <yuekec@gmail.com>
2015-12-07 13:53:11 +0800

    add cart-dish-list component

--- app/public/pc/src/js/components/cart/cart-dish-list.js
+++ app/public/pc/src/js/components/cart/cart-dish-list.js
@@ -1,3 +1,27 @@
-/**
- * Created by ykc on 15/12/7.
- */
+import React from 'react'
+
+var CartDishList = React.createClass({
+
+    render: function () {
+        var {dish, number, subDish, add, del} = this.props;
+        var price = subDish.reduce((sum, el) => el.dish.priceOriginal + sum,
+            dish.priceOriginal)
+        return (
+            <div className="cart-dish">
+                <div className="dish-info">
+                    <img src={dish.cover[0].zh}/>
+                    <span className="main-dish">{dish.title.zh}</span>
+                    {
+                        subDish.map((el) => <span className="sub-dish">{el.dish.title.zh + ' '}</span>)
+                    }
+                </div>
+                <div className="quantity">
+                    <span onClick="del">-</span>{number}<span onClick="add">+</span>
+                </div>
+                <div className="price rmb-char">{price}</div>
+            </div>
+        )
+    }
+});
+
+export default CartDishList;
