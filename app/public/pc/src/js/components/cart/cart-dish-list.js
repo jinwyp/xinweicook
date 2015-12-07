237d42750acc0a8418e5da5fe063fdc83bf3d41e
ykc <yuekec@gmail.com>
2015-12-07 16:12:22 +0800

    update

--- app/public/pc/src/js/components/cart/cart-dish-list.js
+++ app/public/pc/src/js/components/cart/cart-dish-list.js
@@ -1,24 +1,39 @@
 import React from 'react'
+import CartDish from './cart-dish'
 
 var CartDishList = React.createClass({
 
+    _hasStock: function (item, stockWarehouse) {
+        var mDish = item.dish;
+        return item.dish.stockWarehouse[stockWarehouse] > 0
+            && item.subDish.every(it=>this.hasStock(it, stockWarehouse))
+    }
+
     render: function () {
-        var {dish, number, subDish, add, del} = this.props;
-        var price = subDish.reduce((sum, el) => el.dish.priceOriginal + sum,
-            dish.priceOriginal)
+        const {dishList, stockWarehouse} = this.props
+        const props = this.props
+        var selectedAll = dishList.every(item =>
+            item.selected || this._hasStock(item, stockWarehouse))
+        var selectedAll = selectedAll && dishList.some(item => item.selected)
+
         return (
-            <div className="cart-dish">
-                <div className="dish-info">
-                    <img src={dish.cover[0].zh}/>
-                    <span className="main-dish">{dish.title.zh}</span>
+            <div className="cart-dish-list">
+                <div className="header">
+                    <span onClick="props.selectAll" className={selectedAll ? 'fa fa-square-o' : 'fa fa-check-square-o'}></span>
+                    <span>全选</span>
+                    <span>数量</span>
+                    <span>价格</span>
+                </div>
+                <ul className="dish-list">
                     {
-                        subDish.map((el) => <span className="sub-dish">{el.dish.title.zh + ' '}</span>)
+                        dishList.map(item =>
+                            <li key={item._id}>
+
+                                <CartDish key={item._id} {...item} minus={props.minus} plus={props.plus}/>
+                            </li>
+                        )
                     }
-                </div>
-                <div className="quantity">
-                    <span onClick="del">-</span>{number}<span onClick="add">+</span>
-                </div>
-                <div className="price rmb-char">{price}</div>
+                </ul>
             </div>
         )
     }
--- app/public/pc/src/js/components/cart/cart-dish.js
+++ app/public/pc/src/js/components/cart/cart-dish.js
@@ -3,22 +3,25 @@ import React from 'react'
 var CartDish = React.createClass({
 
     render: function () {
-        var {dish, number, subDish, add, del} = this.props;
+        var {dish, number, subDish, selected} = this.props;
+        var props = this.props
         var price = subDish.reduce((sum, el) => el.dish.priceOriginal + sum,
             dish.priceOriginal)
         return (
             <div className="cart-dish">
+                <span onClick="props.select" className={dish.selected ? 'fa fa-check-square-o' : 'fa fa-square-o'}></span>
                 <div className="dish-info">
                     <img src={dish.cover[0].zh}/>
                     <span className="main-dish">{dish.title.zh}</span>
                     {
-                        subDish.map((el) => <span className="sub-dish">{el.dish.title.zh + ' '}</span>)
+                        subDish.map((el) => <span className="sub-dish" key={el.dish._id}>{el.dish.title.zh + ' '}</span>)
                     }
                 </div>
                 <div className="quantity">
-                    <span onClick="del">-</span>{number}<span onClick="add">+</span>
+                    <span onClick="props.minus">-</span>{number}<span onClick="props.plus">+</span>
                 </div>
                 <div className="price rmb-char">{price}</div>
+                <span></span>
             </div>
         )
     }
