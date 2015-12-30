8c93217025750903b960466745c709b33eb50b7f
JinWYP <jinwyp@gmail.com>
2015-12-30 11:56:36 +0800

    fixed

--- app/controllers/orderStatistic.js
+++ app/controllers/orderStatistic.js
@@ -155,9 +155,9 @@ function generateOrderInternalSheetFromArray(worksheet, arrayData){
         for (var rowdish=0; rowdish <= tempDishList.length-1; rowdish++){
 
             currentRow = currentRow + 1;
-            if (typeof tempDishList[rowdish].dish.title.zh == 'undefined'){
-                logger.error(tempDishList[rowdish].dish._id);
-            }
+
+            logger.error(tempDishList[rowdish].dish._id, tempDishList[rowdish].dish.title);
+
             var tempDishName = tempDishList[rowdish].dish.title.zh;
             var cellDishName = {v: tempDishName, t:"s" };
             var cell_refDishName = XLSX.utils.encode_cell({c:0,r:currentRow});
