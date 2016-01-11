3be2c4d2744bf2011ac4f4ba89fd01055514cb7a
JinWYP <jinwyp@gmail.com>
2016-01-11 15:15:50 +0800

    fixed bug

--- app/libs/ksudi.js
+++ app/libs/ksudi.js
@@ -447,7 +447,7 @@ ksuDi.prototype.createFullTimeOrder = function (item, callback){
 
         //console.log('========== KSudi', body);
 
-        logger.error('========== KSudi createFullTimeOrder: ', body);
+        //logger.error('========== KSudi createFullTimeOrder: ', body);
         var result = {};
 
         try{
