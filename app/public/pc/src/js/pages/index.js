7e2e3b085f794abbe5839c37782ff9860ff2aa2b
ykc <yuekec@gmail.com>
2016-01-13 17:42:15 +0800

    add position-selector to eat-list header

--- app/public/pc/src/js/pages/index.js
+++ app/public/pc/src/js/pages/index.js
@@ -3,16 +3,12 @@
 import {init as initCommon, emitter} from './common'
 
 import $ from 'jquery'
-import handlebars from 'handlebars/dist/handlebars'
 import '../utils/jquery-utils'
 
-import {User, Dish, Address} from '../models'
+import {User, Dish} from '../models'
 
 import bindDish from '../utils/bind-dish-operation'
 import {post} from '../utils/xw-fetch'
-import * as tpl from '../dom/templates'
-import Modal from '../dom/modal'
-import * as header from './header'
 // init position selector(react)
 import './position-selector'
 
@@ -22,12 +18,10 @@ var dishes, cart, user, range
 $(document).ready(() => {
     Promise.all([
         User.getUser(),
-        Dish.getList(),
-        Address.range()
+        Dish.getList()
     ]).then(res => {
         user = res[0]
         dishes = res[1]
-        range = res[2]
         cart = user && user.shoppingCart || []
         initCommon(user)
 
@@ -50,6 +44,3 @@ $(document).ready(() => {
         })
     })
 })
-
-var addressSelectionsTpl = handlebars.compile(tpl.addressSelection)
-var streetItemTpl = handlebars.compile(tpl.streetItem)
