83606bbd14af897bbf93ed77a4663734d8a1f1dd
ykc <yuekec@gmail.com>
2016-01-14 11:13:28 +0800

    make index header fixed, add Microsoft Yahei to font-family

--- app/public/pc/src/js/pages/index.js
+++ app/public/pc/src/js/pages/index.js
@@ -43,4 +43,18 @@ $(document).ready(() => {
             $play.show()
         })
     })
+
+    var $header = $('header')
+    var normalHeader = $header.hasClass('normal')
+    $(window).on('scroll', function () {
+        if (window.scrollY > 112) {
+            if (!normalHeader) {
+                $header.addClass('normal')
+                normalHeader = true
+            }
+        } else if (normalHeader){
+            normalHeader = false
+            $header.removeClass('normal')
+        }
+    })
 })
