0cc6c74ae94186483b041bc6ae081cdff4f7e066
ykc <yuekec@gmail.com>
2016-01-20 14:21:04 +0800

    build

---
+++ app/public/pc/dist/why-us.2d9eaea852eb3f5c1684.js
@@ -0,0 +1,1 @@
+webpackJsonp([10],[function(e,n,t){"use strict";function u(e){return e&&e.__esModule?e:{"default":e}}var i,o=t(65),r=t(110),c=t(39),s=u(c);(0,s["default"])(document).ready(function(){Promise.all([r.User.getUser()]).then(function(e){i=e[0],(0,o.init)(i)})})}]);
