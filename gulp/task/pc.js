91fb3bf5fd8f8d031aa65c3754ceca0f1c28d009
ykc <yuekec@gmail.com>
2016-02-16 13:19:17 +0800

    update gulp files

--- gulp/task/pc.js
+++ gulp/task/pc.js
@@ -1,35 +1,8 @@
-var path = require('path');
-var WebpackDevServer = require("webpack-dev-server");
 var fs = require('fs');
 
 var gulp = require('gulp');
-var gutil = require('gulp-util');
-var webpack = require('webpack');
-var sass = require('gulp-sass');
-var rev = require('gulp-rev');
-var sourcemaps = require('gulp-sourcemaps');
-var autoprefixer = require('gulp-autoprefixer');
-var minifyCss = require("gulp-minify-css");
-var shell = require("gulp-shell");
 var replace = require('gulp-replace');
 var del = require('del');
-
-
-gulp.task('pc', ['sass-pc:watch', 'webpack-devServer-pc']);
-
-gulp.task('pc-production', ['pc-replace']);
-
-gulp.task('webpack-pc', function () {
-    shell.task([
-        "webpack --config webpack.build.js -p"
-    ])
-});
-
-gulp.task('webpack-devServer-pc', function () {
-    shell.task([
-        "node node_modules/.bin/webpack-dev-server --inline --port 8081"
-    ])
-})
 
 // 用webpack生成的带hash的文件替换掉webpack-dev-server的字符
 gulp.task('pc-replace', ['clean-view-pc'], function () {
@@ -59,28 +32,6 @@ gulp.task('pc-replace', ['clean-view-pc'], function () {
         }))
         .pipe(gulp.dest('app/views/pc'))
 })
-
-// sass
-gulp.task('sass-pc', function () {
-    return sassBuild()
-});
-gulp.task('sass-pc:watch', function () {
-    return gulp.watch('app/public/pc/src/sass/*.scss', ['sass-pc'])
-});
-gulp.task('sass-pc-production', function () {
-    return sassBuild(true)
-});
-function sassBuild(production) {
-    var buildOption = {
-        outputStyle: production ? 'compressed' : 'nested'
-    };
-    return gulp.src('app/public/pc/src/sass/**/*.scss')
-        .pipe(sourcemaps.init())
-        .pipe(sass(buildOption).on('error', sass.logError))
-        .pipe(autoprefixer({browsers: ['last 3 versions', '> 3% in CN']}))
-        .pipe(sourcemaps.write('./maps'))
-        .pipe(gulp.dest('app/public/pc/src/css/'));
-}
 
 
 // clean
