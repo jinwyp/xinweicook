ec85ca6ddcfc45311e34d219db13e993a50be23e
ykc <yuekec@gmail.com>
2016-02-17 16:23:29 +0800

    build

--- app/public/pc/src/js/actions/position-selector.js
+++ app/public/pc/src/js/actions/position-selector.js
@@ -1,6 +1,4 @@
-import fetch, {post} from "../utils/xw-fetch"
 import * as types from "../constants/ActionTypes"
-import {orderData} from "../utils/order"
 
 export function toggleSelector(state) {
     return {
