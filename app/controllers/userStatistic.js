315d99f518146c2a4587481dd44f1adcd027a00c
JinWYP <jinwyp@gmail.com>
2016-03-04 16:15:58 +0800

    fixed bug

--- app/controllers/userStatistic.js
+++ app/controllers/userStatistic.js
@@ -1898,12 +1898,6 @@ exports.userListOfHaveLastMonthOrder = function(req, res, next) {
 
             });
 
-            console.log(monthList);
-            console.log(userListFirstMonth);
-            console.log(userListSecondMonth);
-            console.log(userListThirdMonth);
-
-
             userListFirstMonth.forEach(function(user){
                 if (userListSecondMonth.indexOf(user) === -1 && userListThirdMonth.indexOf(user) === -1){
                     if ( userList.indexOf(user) === -1){
