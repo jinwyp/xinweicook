73c74ae159bb15827a90387fca6b82c4e73b5c47
JinWYP <jinwyp@gmail.com>
2016-03-10 17:21:51 +0800

    update

--- app/public/mobile/dist2/js/controllers/balance.440b15ce.js
+++
@@ -1,6 +0,0 @@
-angular.module('xw.controllers').controller('balanceCtrl', function ($scope, Balance, Debug) {
-    Balance.balance().then(function (res) {
-        $scope.balance = res.data.balance;
-    }).catch(Debug.promiseErrFn('查询余额失败'));
-});
-//# sourceMappingURL=balance.440b15ce.js.map
