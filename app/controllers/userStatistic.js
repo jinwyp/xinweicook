e833c97ee17bd579e9b8b33bd1664e4567c274c2
JinWYP <jinwyp@gmail.com>
2016-03-15 14:24:51 +0800

    fixed bug

--- app/controllers/userStatistic.js
+++ app/controllers/userStatistic.js
@@ -1013,7 +1013,7 @@ exports.userListAbove4Orders = function(req, res, next) {
 
             var userIdList = resultUser.map(function(user){
                 return user._id.toString();
-            })
+            });
             console.log('userOrderMore7Different111: ', _.difference(userIdList, result['userOrder7'].userListAbove))
             console.log('userOrderMore7Different222: ', _.difference(result['userOrder7'].userListAbove), userIdList )
 
